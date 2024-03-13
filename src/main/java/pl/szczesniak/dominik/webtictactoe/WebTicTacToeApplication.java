dbbdd217836f9fb54970ac79932ed0ed763f9d16
DominikSzczęśniak <dominikszczesniak128@gmail.com>
2024-03-13 17:48:15 +0100

    created project

---
+++ src/main/java/pl/szczesniak/dominik/webtictactoe/WebTicTacToeApplication.java
@@ -0,0 +1,13 @@
+package pl.szczesniak.dominik.webtictactoe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebTicTacToeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebTicTacToeApplication.class, args);
+	}
+
+}
